bd8f3b3069b4104d0ba1198117158b011b1838f7
nina.bohm <nina.boehm@climatepartner.com>
2022-11-13 16:53:05 +0100

    feat: menu to select between methods

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
 import os
 import easygui
 
-msg = "lädt..."
-title = "Interpolation - Numerische Algorithmen"
-choices = ["Test1", "Test2", "PuTTY"]
-reply = easygui.buttonbox(msg,  title, choices = choices)
 
-if reply == "PuTTY":
-    os.system("putty")
-else:
-    print("done")
+def newton():
+    msg = "lädt..."
+    title = "Interpolation - Numerische Algorithmen"
+    choices = ["Test1", "Test2", "PuTTY"]
+    reply = easygui.buttonbox(msg,  title, choices=choices)
+
+    if reply == "PuTTY":
+        os.system("putty")
+    else:
+        print("done")
+
+
+def hermite():
+    print("selected hermite")
+
+
+def menu():
+    print("Please enter 1, 2 3 to choose an option: ")
+
+    print("1) Newton \n2) Hermite \n3) Exit")
+    userInput = input()
+
+    if userInput == "1":
+        newton()
+
+    if userInput == "2":
+        hermite()
+
+    if userInput == "3":
+        print("Goodbye :)")
+        exit()
+
+
+if __name__ == '__main__':
+    menu()
+
