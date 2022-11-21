25bab314d495d9aab7ff85861dec8cfbaebc8a81
Pauline Thiele <Fenstersteuerungber SupportPauline.Thiele@Student.HTW-Berlin.de>
2022-11-21 17:33:04 +0100

    added Stützstellen, w(x)

--- Lagrange.py
+++ Lagrange.py
@@ -1,5 +1,19 @@
-# kommentardsafasfd1234 sdgf
+# Lagrange 
+# Eingabe der Stützstellen
+import math
+import numbers as np        # brauche NumPy-Bibliothek für zeros..
 
-#sdfghhjk
+n = int(input('Anzahl der Stützstellen x, y eingeben:'))
+x = np.zeros ((n))
+y = np.zeros ((n))      # Zero soll alle Elemente auf 0 initialisieren
 
-#as   dföl   kj
+for i in range (n):   # Durchlauf beliebig vieler Werte + ausgabe
+print ('Eingabe der x-Werte')
+x [i] = float(input('x ['str (i) + ']= '))
+print ('Eingabe der y-Werte')               #eingabe der Stützwerte: P
+y[i] = float(input('y [' + str (i) + ']= '))
+
+
+# grad w(x) = n +1, Werte interpolieren
+import scipy.interpolate
+y_interp = scipy.interpolate.interp1d(x,y)
