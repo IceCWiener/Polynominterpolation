6e3acff7f3c453939c0c6b0551d0493aacb9dd94
Konstantin Regenhardt <konstantin.regenhardt@gmx.net>
2022-11-25 14:08:51 +0100

    added div_diff and create_poly methods to Newton

--- Newton.py
+++ Newton.py
@@ -29,30 +29,52 @@ p(x)=a0+a1(x−1)+a2(x−1)(x−2)+a3(x−1)(x−2)(x−3)
 
 import numpy as np
 
-# Setup der Variablen
-x = [1, 2, 3, 4]
-y = [2, 3, 1, 3]
-P = []
+class Newton:
+    # Erstellen von Newtons "Pyramide" um Koeffizienten des Polynoms zu berechnen
+    def divided_differences(self, x, y):
+        n = len(y) # n-1 = Grad des Polynoms
+        pyramid_matrix = np.zeros([n, n]) # n mal n Array gefüllt mit nullen
+        pyramid_matrix[::,0] = y # erste Spalte wird mit y-Werten gefüllt
+        for j in range(1,n):
+            for i in range(n-j):
+                # Spalten werden nacheinander von berechneten Koeffizienten gefüllt
+                pyramid_matrix[i][j] = (pyramid_matrix[i+1][j-1] - pyramid_matrix[i][j-1]) / (x[i+j] - x[i])
+        #print(pyramid_matrix)
+        
+        return pyramid_matrix[0] # erste Reihe wird zurückgegeben
 
-for i in x:
-    point = [x[i-1], y[i-1]]
-    P.append(point)
+    def create_polynome(self, coefficients):
+        poly = ""
+        degree = len(coefficients) - 1
+        coefficients =  self.flip_array(coefficients)
 
-# Berechnen der a-Werte
-def calcA(x, y):
-    n = len(y)
-    a = np.zeros([n,n]) # erstellt ein n mal n großes Array gefüllt mit 0
+        for i in coefficients:
+            if degree > 1:
+                poly = poly + "(" + str(i) + ")x" + str(degree) + " + "
+            elif degree == 1:
+                poly = poly + "(" + str(i) + ")x "
+            else:
+                poly = poly + " + (" + str(i) + ")"
+            
+            degree = degree - 1
+        return poly
 
-    for i in range(n):
-        a[0, i] = y[i]
-
-    print(a)
-
-
-# main
-def main():
-    calcA(x, y)
+    def flip_array(self, arr):
+        result = []
+        
+        for i in range(len(arr) - 1, -1, -1):
+            result.append(arr[i])
+        
+        return result
 
 
-main()
-exit()
+
+    # Pyramide anschaulicher machen wie in der Mitschrift der Vorlesung
+    # Namen ändern & refactorn
+
+x = np.array([1, 7, 3, 5])
+y = np.array([1, 9, 2, 8])
+nt = Newton()
+div_diff = nt.divided_differences(x, y)
+#print(div_diff)
+print(nt.create_polynome(div_diff))
