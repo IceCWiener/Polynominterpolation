e468145bf2a163abbee1b39179a4ade9c6ee7b23
Pauline Thiele <Fenstersteuerungber SupportPauline.Thiele@Student.HTW-Berlin.de>
2022-11-30 20:25:08 +0100

    chagend variables, added output

--- Lagrange Python_Komplett.py
+++ Lagrange Python_Komplett.py
@@ -1,24 +1,11 @@
 # Lagrange 
-# Eingabe der Stützstellen
-import math
+import math # brauch man das?
 import numpy as np       
 
-n = int(input('Anzahl der Stützstellen x, y eingeben:'))
-x = np.zeros ((n))
-y = np.zeros ((n))      # Zero soll alle Elemente auf 0 initialisieren
-
-
-for i in range (n):   # Durchlauf beliebig vieler Werte + ausgabe
-    print ('Eingabe der x-Werte')
-    x[i] = float(input('x [' + str (i) + ']= '))
-    print ('Eingabe der y-Werte')               #eingabe der Stützwerte: P
-    y[i] = float(input('y [' + str (i) + ']= '))
-
-
-# funktioniert bis jetzt nur für 3 Stützstellen/Stützwerte 
-
-stützstellen = [1, 2, 3]
-stützwerte = [5, 0, -4]
+stützstellen = []
+#stützstellen = [1, 2, 3]
+#stützwerte = [5, 0, -4]
+stützwerte = []
 
 w_function = []
 li_x_wert_teil = 1
@@ -26,6 +13,24 @@ li_x_wert = 1
 alle_li_x_werte = []
 alle_li_function =[]
 alle_Li_function = []
+
+
+# Eingabe der Stützstellen
+n = int(input('Wie viele Stützstellen soll es geben?')) 
+x = np.zeros ((n))
+y = np.zeros ((n))      # Zero soll alle Elemente auf 0 initialisieren
+
+# Durchlauf beliebig vieler Werte + Ausgabe
+for i in range (n):   
+    print ('Eingabe der x-Werte')
+    x = float(input('x [' + str (i) + ']= '))
+    stützstellen.append(x)
+    print ('Eingabe der y-Werte')
+    y = float(input('y [' + str (i) + ']= '))
+    stützwerte.append(y)
+
+print("Stützstellen: ", stützstellen)
+print("Stützwerte: ", stützwerte)
 
 # Stützstellen mit negativem Vorzeichen erzeugen
 for a in range(len(stützstellen)):
@@ -52,33 +57,39 @@ print("\nli_x_Werte", alle_li_x_werte, "mit li_function", alle_li_function, "\n"
 
 #Erstellen des Arrays, der alle Werte der li funktionen und lix werte beinhaltet
 
-Li_array = np.zeros((len(alle_li_x_werte),2,len(alle_li_x_werte) - 1)) #3, 2, 2
+Li_array = np.zeros((n,2,n - 1)) #3, 2, 2 für n=3; (2,2,1) für n=2
 
 # Einfügen der li_function Werte in den Array 
-for t in range(len(alle_li_x_werte)):
-    Li_array = np.insert(Li_array, t*4, alle_li_function[t])
+for t in range(n):
+    Li_array = np.insert(Li_array, t*(2*(n - 1)), alle_li_function[t]) # t*2 für n=2; t*4 für n=3; t*6 für n=4
 #dann ist der restliche Array mit Nullen gefüllt, diese werden aber bei resize ignoriert 
 
 # Einfügen der li_x_werte in den Array 
-for u in range(len(alle_li_x_werte)):
-    Li_array = np.insert(Li_array, (u*4)+2, alle_li_x_werte[u])
-    Li_array = np.delete(Li_array, [(u*4)+3], None) 
+for u in range(n):
+    Li_array = np.insert(Li_array, (u*(2*(n - 1)))+(n-1), alle_li_x_werte[u]) # (u*2)+1 bei n=2; (u*4)+2 bei n=3; (u*6)+3 bei n=4
+    Li_array = np.delete(Li_array, [(u*(2*(n - 1)))+n]) # (u*2)+2 bei n=2; (u*4)+3 bei n=3; (u*6)+4 bei n=4
 
 # resize Array 
-Li_array = np.resize(Li_array,(len(alle_li_x_werte),2,len(alle_li_x_werte)-1)) #3,2,2
+Li_array = np.resize(Li_array,(n,2,n-1)) #3,2,2 bei n=3
 print("fertiger array: ", Li_array)
 
 
 
 # Li_array aumultiplizieren nach dem Schema: (x + a)*(x + b) = x^2 + x*(a + b) + a*b
 
-array_ausmult = np.zeros((1,len(alle_li_x_werte))) #0,0,0
-print(array_ausmult)
-for d in range(len(alle_li_x_werte)): 
-    for e in range(len(alle_li_x_werte) -1):
-        wert_ausmultipliziert = np.array([[1 / Li_array[d,e+1,e], (Li_array[d,e,e] + Li_array[d,e,e+1]) / Li_array[d,e+1,e], (Li_array[d,e,e] * Li_array[d,e,e+1]) / Li_array[d,e+1,e]]])
-        print("Wert ausmultipliziert: ",wert_ausmultipliziert)
-        array_ausmult = np.concatenate((array_ausmult, wert_ausmultipliziert))
+array_ausmult = np.zeros((1,n)) #0,0,0 bei n=3
+
+for d in range(n): 
+    for e in range(n -1):
+        if n == 2:
+            print("durch: ", Li_array[d,e+1,e])
+            wert_ausmultipliziert = np.array([[1 / Li_array[d,e+1,e], Li_array[d,e,e] / Li_array[d,e+1,e]]])
+            print("Wert ausmultipliziert: ",wert_ausmultipliziert)
+            array_ausmult = np.concatenate((array_ausmult, wert_ausmultipliziert))
+        elif n == 3:
+            wert_ausmultipliziert = np.array([[1 / Li_array[d,e+1,e], (Li_array[d,e,e] + Li_array[d,e,e+1]) / Li_array[d,e+1,e], (Li_array[d,e,e] * Li_array[d,e,e+1]) / Li_array[d,e+1,e]]])
+            print("Wert ausmultipliziert: ",wert_ausmultipliziert)
+            array_ausmult = np.concatenate((array_ausmult, wert_ausmultipliziert))
         break
 
 print(array_ausmult)
@@ -86,16 +97,24 @@ print(array_ausmult)
 # Polynom berechnen mit Stützwerten * Li Werte 
 polynom = []
 for f in range(1, len(array_ausmult)):
-    for g in range(3):
+    for g in range(n):
         polynomteil = stützwerte[f-1] * array_ausmult[f,g]
         polynom.append(polynomteil)
-print(polynom)    
+print("Polynom: ",polynom)    
 
-# Polynom sortieren und ausgeben
+# Polynom sortieren 
 polynom_ausgabe = []
-for h in range(3):
-    polynom_ausgabe_teil = polynom[h] + polynom[h+3] + polynom[h+6]
+for h in range(n):
+    polynom_ausgabe_teil = polynom[h] + polynom[h+n] + polynom[h+n*2] # ist nur für n=3 gültig 
     polynom_ausgabe.append(polynom_ausgabe_teil)
-print("Ausgabe: p(x) =", polynom_ausgabe[0], "x^2 +", polynom_ausgabe[1], "x +", polynom_ausgabe[2])
+print("fertiges Polynom:", polynom_ausgabe)
 
-
+# Ausgabe des Polynoms
+ausgabe = []
+ausgabe_teil = str([])
+for i in range(len(polynom_ausgabe)):
+    ausgabe_teil = polynom_ausgabe[i] , "x^" , len(polynom_ausgabe)-i-1
+    ausgabe.append(ausgabe_teil)
+    if i < len(polynom_ausgabe)-1:
+        ausgabe.append("+")
+print("Ausgabe: p(x) =", ausgabe)
