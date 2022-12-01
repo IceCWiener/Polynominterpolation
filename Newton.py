db95eb67ee4cd5aa2d6db4f1d4581743fae4b91c
Konstantin Regenhardt <konstantin.regenhardt@gmx.net>
2022-12-01 20:24:54 +0100

    added int poly calc

--- Newton.py
+++ Newton.py
@@ -1,3 +1,5 @@
+import math
+
 class Newton:
     # Erstellen von Newtons "Pyramide" um Koeffizienten/Multiplikatoren des Polynoms zu berechnen
     def divided_differences(self, x_values, y_values):
@@ -16,7 +18,7 @@ class Newton:
         for i in range(n):
             pyramid_matrix[i][0] = float(y_values[i])
 
-        print(pyramid_matrix)
+        #print(pyramid_matrix)
         
         for j in range(1,n):
             for i in range(n-j):
@@ -27,12 +29,25 @@ class Newton:
         return pyramid_matrix[0] # erste Reihe wird zurückgegeben
 
     # TODO: Funktionalität für Int-Polynom erstellen (zum Ausrechnen)
-    def create_string_polynomial(self, coefficients):
-        str_poly = ""
-        degree = len(coefficients) - 1
-        coefficients =  self.flip_array(coefficients)
+    def calculate_poly(self, coeffs, x_vals):
+        count = len(coeffs)
+        result = []
 
-        for i in coefficients:
+        # Rechnung für jeden angegeben x-Wert
+        for i in range(len(x_vals)):
+            calc = 0.
+            for j in range(count):
+                calc = calc + pow(x_vals[i], j)*coeffs[j]
+            result.append(calc)
+
+        return result
+
+    def create_string_polynomial(self, coeffs):
+        str_poly = ""
+        degree = len(coeffs) - 1
+        coeffs =  self.flip_array(coeffs)
+
+        for i in coeffs:
             if degree > 1:
                 str_poly = str_poly + "(" + str(i) + ")*x**" + str(degree) + " + "
             elif degree == 1:
@@ -54,7 +69,8 @@ class Newton:
 x_vals = [-1, 0, 1, 2]
 y_vals = [1, 1, 2, 0]
 nt = Newton()
-div_diff = nt.divided_differences(x_vals, y_vals)
+coefficients = nt.divided_differences(x_vals, y_vals)
 #print(div_diff)
-string_poly = nt.create_string_polynomial(div_diff)
-#print(string_poly)
+string_poly = nt.create_string_polynomial(coefficients)
+print(string_poly)
+print(nt.calculate_poly(coefficients, x_vals=[-1]))
