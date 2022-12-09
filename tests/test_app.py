c3ecb41c5e57c7b383a0c9edb7a2e73912f55da1
nina.bohm <nina.boehm@climatepartner.com>
2022-12-09 18:18:18 +0100

    multiply some modification

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,7 @@
 import unittest
 from app import generate_polynom_with_brackets
 from app import generate_polynom_coefficients
+from app import multiply_polynoms
 
 class TestApp(unittest.TestCase):
     def test_should_return_bracket_polynom_when_two_sampling_points(self):
@@ -21,13 +22,7 @@ class TestApp(unittest.TestCase):
         expected = "1.0 + 4.0(x-1) + -2.0(x-1)(x-1) + 1.0(x-1)(x-1)(x-2) + 1.0(x-1)(x-1)(x-2)(x-2)"
         self.assertEqual(expected, generate_polynom_with_brackets(pyramid_matrix, x_values))
 
-    def test_should_return_polynom_coefficients_when_second_grade(self):
-        pyramid_matrix = [[1.0, 4.0, -2.0], [1.0, 2.0, -1.0], [3.0, 1.0, 1.0]]
-        x_values = [1, 1, 2]
-        expected = [-5.0, 8.0, -2.0]
-        self.assertEqual(expected, generate_polynom_coefficients(pyramid_matrix[0], x_values))
-
-    # def test_should_return_multiplied_out_polynom_when_three_sampling_points(self):
+        # def test_should_return_multiplied_out_polynom_when_three_sampling_points(self):
     #     pyramid_matrix = [
     #         [1.0, 4.0, -2.0, 1.0, 1.0],
     #         [1.0, 2.0, -1.0, 2.0, 0.0],
@@ -40,6 +35,20 @@ class TestApp(unittest.TestCase):
     #     polynom_with_brackets = "1.0 + 4.0(x-1) + -2.0(x-1)(x-1) + 1.0(x-1)(x-1)(x-2) + 1.0(x-1)(x-1)(x-2)(x-2)"
     #     expected = "x^4 + -5x^3 + 7x^2 + x + -3"
     #     self.assertEqual(expected, generate_multiplied_out_polynom(pyramid_matrix, x_values))
+
+    def test_should_return_polynom_coefficients_when_second_grade(self):
+        pyramid_matrix = [[1.0, 4.0, -2.0], [1.0, 2.0, -1.0], [3.0, 1.0, 1.0]]
+        x_values = [1., 1., 2.]
+        expected = [-5.0, 8.0, -2.0]
+        self.assertEqual(expected, generate_polynom_coefficients(pyramid_matrix[0], x_values))
+
+    def test_should_return_multiplied_polynom(self):
+        pol1 = [1, 5, 2]
+        pol2 = [6, 1, 4, 3]
+        expected = [6, 31, 21, 25, 23, 6]
+        self.assertEqual(expected, multiply_polynoms(pol1, pol2))
+
+
 
 
 if __name__ == '__main__':
