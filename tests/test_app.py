f9f0398a7bf26678ccd30d15563bde0c37ca4979
nina.bohm <nina.boehm@climatepartner.com>
2022-12-13 13:49:14 +0100

    fix: tests with utility

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,51 +3,55 @@ from utility import Utility
 
 
 class TestApp(unittest.TestCase):
-    def __init__(self):
-        self.util = Utility()
-
     def test_should_return_polynom_coefficients_when_second_grade(self):
+        util = Utility()
         pyramid_matrix = [[1.0, 4.0, -2.0], [1.0, 2.0, -1.0], [3.0, 1.0, 1.0]]
         x_values = [1, 1, 2]
         expected = [-5.0, 8.0, -2.0]
-        self.assertEqual(expected, self.util.generate_polynom_coefficients(
+        self.assertEqual(expected, util.generate_polynom_coefficients(
             pyramid_matrix[0], x_values))
 
     def test_should_return_multiplied_polynom(self):
+        util = Utility()
         pol1 = [1, 5, 2]
         pol2 = [6, 1, 4, 3]
         expected = [6, 31, 21, 25, 23, 6]
-        self.assertEqual(expected, self.util.multiply_polynoms(pol1, pol2))
+        self.assertEqual(expected, util.multiply_polynoms(pol1, pol2))
 
     def test_should_return_polynom_with_multiplied_elements(self):
+        util = Utility()
         pol1 = [1, 5, 2]
         factor = 4
         expected = [4, 20, 8]
         self.assertEqual(
-            expected, self.util.multiply_each_element_of_polynom(pol1, factor))
+            expected, util.multiply_each_element_of_polynom(pol1, factor))
 
     def test_should_return_added_polynoms(self):
+        util = Utility()
         pol1 = [1, 5, 2]
         pol2 = [6, 1, 18]
         expected = [7, 6, 20]
-        self.assertEqual(expected, self.util.add_polynoms(pol1, pol2))
+        self.assertEqual(expected, util.add_polynoms(pol1, pol2))
 
     def test_should_return_added_polynoms_when_one_empty(self):
+        util = Utility()
         pol1 = []
         pol2 = [6, 1, 18]
-        self.assertEqual(pol2, self.util.add_polynoms(pol1, pol2))
+        self.assertEqual(pol2, util.add_polynoms(pol1, pol2))
 
     def test_should_return_added_polynoms_when_one_element(self):
+        util = Utility()
         pol1 = [1]
         pol2 = [8, 1, 18]
         expected = [9, 1, 18]
-        self.assertEqual(expected, self.util.add_polynoms(pol1, pol2))
+        self.assertEqual(expected, util.add_polynoms(pol1, pol2))
 
     def test_should_return_standard_polynom(self):
+        util = Utility()
         coeffiecients = [-3.0, 1.0, 7.0, -5.0, 1.0]
         expected = "p(x) = 1.0 x^4 + -5.0 x^3 + 7.0 x^2 + 1.0 x^1 + -3.0 x^0 + "
         self.assertEqual(
-            expected, self.util.transform_coefficients_to_pretty_polynom(coeffiecients))
+            expected, util.transform_coefficients_to_pretty_polynom(coeffiecients))
 
 
 if __name__ == '__main__':
