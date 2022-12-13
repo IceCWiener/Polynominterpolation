0042f8028bfd0c881abe5f940762deb0965310ce
Konstantin Regenhardt <konstantinlucius.regenhardt@cgi.com>
2022-12-13 16:28:16 +0100

    activated console input

--- app.py
+++ app.py
@@ -6,19 +6,20 @@ from utility import Utility
 
 def collect_xy_values():
     xy_values = []
-    hermite_var = False
     i = 0
-    # while True:
-    #     if i != 0:
-    #         check_if_wants_to_continue = input("möchten Sie eine " + str(i + 1) + "te Stützstelle eingeben bitte Enter drücken, andernfalls geben sie n ein: ")
-    #         if check_if_wants_to_continue == "n":
-    #             break
-    #     sampling_point = (int(input("Bitte X-wert der " + str(i + 1) + "ten Stützstelle eingeben: ")), int(input("Bitte Y-wert der " + str(i + 1) + "ten Stützstelle eingeben: ")))
-    #     sampling_points_list.append(sampling_point)
-    #     i += 1
+    while True:
+        if i != 0:
+            check_if_wants_to_continue = input("möchten Sie einen " + str(
+                i + 1) + "te Stützpunkt eingeben bitte Enter drücken, andernfalls geben sie n ein: ")
+            if check_if_wants_to_continue == "n":
+                break
+        xy_point = (int(input("Bitte Stützstelle des " + str(i + 1) + "ten Punktes eingeben: ")),
+                    int(input("Bitte Stützwert des " + str(i + 1) + "ten Punktes eingeben: ")))
+        xy_values.append(xy_point)
+        i += 1
 
-    # xy_values = [(1, 1), (1, 4), (2, 3), (2, 1), (2, 2)]  # Hermite
-    xy_values = [(1, 1), (3, 4), (232, 3), (5, 155), (1, 2)]  # Else
+    # xy_values = [(1, 1), (13, 4), (2, 3), (2, 1), (1, 2)]  # Hermite
+    # xy_values = [(1, 1), (3, 4), (232, 3), (5, 155), (432, 2)]  # Else
     # Newton Test -> (11 x^3)/72 - (145 x^2)/72 + (85 x)/12 - 3
     #xy_values = [(0, -3), (6, 0), (8, 3), (9, 9)]
     #xy_values = [(1, 5), (2, 1), (3, -4)]
@@ -33,21 +34,20 @@ def create_polynom(xy_values):
     y_values = util.get_y_values(xy_values)
 
     if use_hermite:
-        print("wir machen Hermit")
+        print("wir machen Hermit\n")
         hermite_polynom = Hermite()
         herm_coef = hermite_polynom.get_coefficients(xy_values)
         standard_form_polynom_coef = util.generate_polynom_coefficients(
             herm_coef, x_values)
         standard_form_polynom_coef = util.round_list(
             standard_form_polynom_coef, 3)
-        pretty_polynom = util.create_string_polynomial(
-            standard_form_polynom_coef)
-        # print(pretty_polynom)
+        pretty_polynom = "Hermite: " + util.create_string_polynomial(
+            standard_form_polynom_coef) + "\n"
 
         return [pretty_polynom]
 
     else:
-        print("wir machen Newton und Lagrange")
+        print("wir machen Newton und Lagrange\n")
         newt = Newton()
 
         # Lagrange
@@ -61,7 +61,7 @@ def create_polynom(xy_values):
         norm_poly = lagrange_polynom.normalform_poly(xy_len, polynom)
         norm_poly = util.round_list(norm_poly, 3)
         pretty_polynom = "Lagrange: " + util.create_string_polynomial(
-            norm_poly)
+            norm_poly) + "\n"
 
         # Newton
         newt_coeffs = newt.newton_div_diff(x_values, y_values)
@@ -70,7 +70,6 @@ def create_polynom(xy_values):
         poly_coeffs = util.round_list(poly_coeffs, 3)
         pretty_newt = "Newton: " + util.create_string_polynomial(
             poly_coeffs)
-        #print("\n\nNewton's Polynome: " + pretty_newt)
 
         return [pretty_newt, pretty_polynom]
 
@@ -79,7 +78,7 @@ if __name__ == '__main__':
     util = Utility()
 
     xy_values = collect_xy_values()
-    print(f'Stützstellen: {xy_values}')
+    print(f'\nStützpunkte: {xy_values}\n')
     polynom = create_polynom(xy_values)
     for i in polynom:
         print(i)
