ca79d5a53f5fcb8215b14ef10767032ff602a4fc
nina.bohm <nina.boehm@climatepartner.com>
2022-12-13 15:30:47 +0100

    test: Li function lagrange

--- tests/test_lagrange.py
+++ tests/test_lagrange.py
@@ -10,12 +10,3 @@ class TestLagrange(unittest.TestCase):
         x_vlaues = [1, 2, 3]
         Li_function = lagrange.create_li_function(x_vlaues)[0]
         self.assertEqual(expected, Li_function)
-
-    # def test_should_return_polynom_when_input_n_Li_function(self):
-    #     lagrange = Lagrange()
-    #     expected = [15, -12, 5, 2.5, -3, 4, -1, -4, 6, -2]
-    #     n = 3
-    #     alle_li_function = [6, -5, 1, 3, -4, 1, 2, -3, 1]
-    #     alle_li_x_werte = [1, 2, 3]
-    #     Li_function = lagrange.create_Li_function(n, alle_li_function, alle_li_x_werte)
-    #     self.assertEqual(expected, Li_function)
